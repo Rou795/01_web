77096bc0797a71ddaed52ebfc797016d0351480f
Evgeniy Kukarskiy <jon.79555@gmail.com>
2024-04-16 22:58:21 +0700

    Исправил ошибку в коде из прошлого домашнего задания. Не нужно проверять окончание текста для чтения из потока методом ready.

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -3,15 +3,18 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+
+import static java.lang.Thread.sleep;
 
 public class Server {
 
     static int port = 9999;
 
     volatile static HashMap<String, Handler> handlers = new HashMap<>();
-
+    String fullRequest = "";
     static List<String> messages = new ArrayList<>();
 
     public void initial() {
@@ -27,66 +30,88 @@ public class Server {
                     ) {
                         // Читаем весь запрос и раскладываем его по частям в объект Request
                         StringBuilder builder = new StringBuilder();
-                        while (in.ready()) {
-                            builder.append(in.readLine());
-                            builder.append("\n");
-                        }
-                        String fullRequest = builder.toString();
-                        System.out.println(fullRequest);
-                        String body = "";
-                        //проверка на существование body
-                        if (fullRequest.contains("\r\n\r\n")) {
-                            body = fullRequest.split("\r\n\r\n")[1];
-                            fullRequest = fullRequest.replaceFirst(body, "");
-                        }
-                        String[] parts = fullRequest.split("\n");
-                        HashMap<String, String> headers = new HashMap<>();
-                        String requestLine = "";
-                        for (int i = 0; i < parts.length; i++) {
-                            if (i == 0) {
-                                requestLine = parts[i];
+                        String bufferTest = "";
+                        while (true) {
+                            bufferTest = in.readLine();
+                            if (bufferTest.isEmpty()) {
+                                break;
                             } else {
-                                String[] pair = parts[i].split(":", 2);
-                                headers.put(pair[0], pair[1].strip());
+                                System.out.println(bufferTest);
+                                builder.append(bufferTest);
+                                builder.append("\n");
                             }
                         }
-                        //выделение первой строки запроса для создания объекта Request
-                        System.out.println(requestLine);
-                        String method;
-                        String path;
-                        String protocol;
-                        final String[] partsForBuilder = requestLine.split(" ");
-                        synchronized (partsForBuilder) {
-                            method = partsForBuilder[0];
-                            path = partsForBuilder[1];
-                            protocol = partsForBuilder[2];
-                        }
-                        Request request = new Request(method, path, protocol, headers, body);
-                        System.out.println("good connect");
-                    // просмотр handler'ов по ключу и задействование нужного
-                        synchronized (handlers) {
-                            String findKey = request.getMethod() + request.getPath();
-                            for (String key : handlers.keySet()) {
-                                if (key.equals(findKey)) {
-                                    handlers.get(key).handle(request, out);
+                        String body = "";
+                        fullRequest = builder.toString();
+                        if (fullRequest.isEmpty()) {
+
+                            System.out.println(fullRequest + "fullRequest is empty");
+                        } else {
+                            System.out.println(fullRequest);
+
+                            //проверка на существование body
+                            if (fullRequest.contains("\r\n\r\n")) {
+                                body = fullRequest.split("\r\n\r\n")[1];
+                                fullRequest = fullRequest.replaceFirst(body, "");
+                            }
+                            String[] parts = fullRequest.split("\n");
+
+                            HashMap<String, String> headers = new HashMap<>();
+                            String requestLine = "";
+                            for (int i = 0; i < parts.length; i++) {
+                                if (i == 0) {
+                                    requestLine = parts[i];
+                                } else {
+                                    String[] pair = parts[i].split(":", 2);
+                                    headers.put(pair[0], pair[1].strip());
                                 }
                             }
-                            out.write(headersResponse404());
-                            out.flush();
+                            //выделение первой строки запроса для создания объекта Request
+                            System.out.println(requestLine);
+                            String method = "";
+                            String path = "";
+                            String protocol = "";
+                            final String[] partsForBuilder = requestLine.split(" ");
+                            try {
+                                synchronized (partsForBuilder) {
+                                    method = partsForBuilder[0];
+                                    path = partsForBuilder[1];
+                                    protocol = partsForBuilder[2];
+                                }
+                            } catch (ArrayIndexOutOfBoundsException ex) {
+                                System.out.println("array empty");
+                                System.out.println(Arrays.toString(partsForBuilder));
+                            }
+                            Request request = new Request(method, path, protocol, headers, body);
+                            System.out.println("good connect");
+                            // просмотр handler'ов по ключу и задействование нужного
+                            synchronized (handlers) {
+                                String findKey = request.getMethod() + request.getPath();
+                                for (String key : handlers.keySet()) {
+                                    if (key.equals(findKey)) {
+                                        handlers.get(key).handle(request, out);
+                                    }
+                                }
+                                out.write(headersResponse404());
+                                out.flush();
+                            }
                         }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+                        } catch(IOException e){
+                            throw new RuntimeException(e);
+                        }
                     }
+            } ;
+                for (int i = 0; i < 64; i++) {
+                    connectPool.add(new Thread(connect));
+                    connectPool.get(i).start();
                 }
-            };
-            for (int i = 0; i < 64; i++) {
-                connectPool.add(new Thread(connect));
-                connectPool.get(i).start();
-                connectPool.get(i).join();
+                for (Thread thread : connectPool) {
+                    thread.join();
+                }
+            } catch(IOException | InterruptedException e){
+                e.printStackTrace();
             }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+
     }
 
     public static byte[] headersResponse(int code, String mimeType, long length) {
